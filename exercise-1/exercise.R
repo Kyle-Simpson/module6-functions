4b2a35013032ecffee305e9456a7daca4ccb69d6
Kyle Simpson <kes97@uw.edu>
2017-01-10 14:00:31 -0800

    Complete exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,16 +1,24 @@
 # Exercise 1: writing and executing functions
 
+#completed
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
-
+AddThree <- function(initial.number) {
+  new.number <- initial.number + 3
+  return(new.number)
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
 # The function should return the total length in meters
-
+ImperialToMetric <- function(number.of.feet, number.of.inches) {
+  meters <- (number.of.feet * 0.3) + (number.of.inches * 0.025)
+  return(meters)
+}
 
 # Create a variable `height.in.meters` by passing your height in imperial to the
 # `ImperialToMetric` function
+height.in.meters <- ImperialToMetric(6, 0)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,16 +1,34 @@
 # Exercise 2: writing and executing functions (II)
 
+#completed
 # Define a function `CompareLength` that takes in 2 character strings, and returns
 # the sentence of the form
 #   "The difference in length is N"
-
+CompareLength <- function(char1, char2) {
+  dif <- nchar(char1) - nchar(char2)
+  
+  result <- paste("The difference in length is", dif)
+  print(result)
+}
 
 # Pass two strings of different lengths to your `CompareLength` function
-
+CompareLength("This", "Those")
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
-
+DescribeDifference <- function(string1, string2) {
+  if (nchar(string1) > nchar(string2)) {
+    dif <- nchar(string1) - nchar(string2)
+    res <- paste("Your first string is longer by", dif, "characters")
+    print(res)
+  } else {
+    dif <- nchar(string2) - nchar(string1)
+    res <- paste("Your second string is longer by", dif, "characters")
+    print(res)
+  }
+  
+}
 
 # Pass two strings of different lengths to your `DescribeDifference` function
+DescribeDifference("This", "Those")
